49e3b65c5813b9af661facb9881bfcc16c03ffc2
limadanillo <danilolima@dcc.ufba.br>
2017-11-01 11:46:29 -0200

    Commit inicial
    Parte léxica do semestre 2017.1

---
+++ src/tokens.h
@@ -0,0 +1,37 @@
+/* Lista dos tokens, com valores constantes associados.
+ * Este arquivo tera vocacao a ser incluido em varios outros.
+ * A escolha dos valores constantes eh totalmente arbitraria.
+ * Cada valor deve ser distinto...
+ */
+#define INT 260
+#define DOUBLE 261
+#define FLOAT 262
+#define CHAR 263
+#define QUOTE 264
+#define DQUOTE 265
+#define LE 266
+#define GE 267
+#define EQ 268
+#define NE 269
+#define AND 270
+#define OR 271
+#define NOT 272
+#define IF 273
+#define THEN 274
+#define ELSE 275
+#define WHILE 276
+
+#define IDF 300
+#define INT_LIT 301
+#define F_LIT 302
+
+//Minhas defines
+#define SWAP 304
+#define FOR 305
+#define NEXT 306
+#define END 307
+#define TRUE 308
+#define FALSE 309 
+#define CONST 310
+#define STR_LIT 311
+#define STRING 312
